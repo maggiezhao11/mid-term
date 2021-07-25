6731c0fca6092e908d2e86948b6cab1e8dea9abd
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-25 19:46:39 +0000

    add again

---
+++ db/seeds/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, name, password, email) VALUES
+(1, 'Amy', 123, 'amy@mail.com'),
+(2, 'Bob', 123, 'bob@mail.com'),
+(3, 'Cindy', 123, 'cindy@mail.com'),
+(4, 'David', 123, 'david@mail.com'),
+(5, 'Francise', 123, 'francise@mail.com');
