a918c4e25d554d6b2cf27f867fc112a34964d891
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-26 21:05:48 +0000

    add myResources routes and ejs

--- routes/my_resources.js
+++ routes/my_resources.js
@@ -9,11 +9,19 @@ const express = require('express');
 const router  = express.Router();
 
 const myResources = (db) => {
-  router.get("/my-resources", (req, res) => {
-    db.query(`SELECT * FROM resources JOIN user_likes ON owner_id = user_likes.user_id WHERE owner.id = $1`, [req.params.user_id]) // need to console.log the incoming data to finalize the variable name
+  router.get("/:user_id", (req, res) => {
+    db.query(`SELECT * FROM resources WHERE resources.owner_id = $1`, [req.params.user_id]) // the name after params is the same one after the /:
       .then(data => {
-        const users = data.rows;
-        res.render("my-resources");//it should be able to render the same page of "resources" but use filter function to the list.
+        const myResources = data.rows;
+        console.log("data.rows:++++++++", data.rows);
+        const templateVars = {resources: myResources}
+        db.query(`SELECT * FROM user_likes JOIN resources ON resources.id = user_likes.resource_id WHERE user_likes.owner_id = $1`, [req.params.user_id])
+      .then(result => {
+        console.log("result:----------", result.rows);
+        templateVars.user_likes = result.rows
+        res.render("my-resources", templateVars);//it should be able to render the same page of "resources" but use filter function to the list.
+
+        })
       })
       .catch(err => {
         res
